3eb1907d68a437cd35e2b0007ddc5f0ffaab8d16
badistighlit <121290363+badistighlit@users.noreply.github.com>
2024-01-27 12:34:21 +0100

    RDV 27 01 2024

--- src/adapters/input/CreationRevisionService.js
+++ src/adapters/input/CreationRevisionService.js
@@ -3,3 +3,10 @@ import { CardManager } from "../../Domaine/ports/input/CardManager";
 import { Card } from "../../Domaine/Entites/Card"; 
 import { Cards } from "../../Domaine/Entites/Cards";
 
+export class CreationRevesionService{
+
+        
+
+
+
+}
